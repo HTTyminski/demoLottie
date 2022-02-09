bf748796151fcf3304fe51613071a9bbea5c4449
HTTyminski <tyminski2011@gmail.com>
2022-02-08 22:28:04 -0300

    new lottie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useRef} from 'react';
+import lottie from 'lottie-web';
+
 
 function App() {
+
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('./rocket.json')
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Lottie Demo</h1>
+      <div className='container' ref={container}></div>
+
+
     </div>
   );
 }
